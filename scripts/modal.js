a7ac69b018f71bc9152abef1f52081872390debb
Tarunkcode <tarunkumar43461@gmail.com>
2021-10-26 20:54:04 +0530

    initial commit

---
+++ scripts/modal.js
@@ -0,0 +1,137 @@
+$(document).ready(function () {
+  // MODAL
+  var modalText = {
+    // discover: {
+    //   title: "ChowNow Discover",
+    //   tag: "FOOD ORDERING PLATFORM.",
+    //   detail:
+    //     "ChowNow Discover is a platform that lets customers discover new local restaurants and provides business owners with tools to convert first time orders into lifelong diners.",
+    //   link: "https://natureguardianz.me",
+    // },
+    ordering: {
+      title: "Food Ordering Web",
+      tag: "ONLINE ORDERING SOLUTION.",
+      detail:
+        "This is not uploaded right now still is progress to made. This is a commission-free online ordering system and food ordering app helping restaurants feed their hungry customers.",
+      link: "https://xyz.com",
+    },
+    nature: {
+      title: "Nature Guardian",
+      tag: "An NGO platform from India",
+      detail:
+        "Nature Guardian charitable Foundation (NGCF) is a private indian organisation running by a group indian citizens, our area of interest is in wildlife and nature conservation we are funded by indian, american, france, and britain governments and many other billionaire buisnessmens from all over the globe so far. we are recognised in 23 different contries all over the globe.",
+      link: "https://natureguardianz.me",
+    },
+    player: {
+      title: "Video Player",
+      tag: "Video player controllers in javascript",
+      detail:
+        "This is my mini project in my college called video player in javascript, when I was very naive in web development",
+      link: "https://natureguardianz.me/video-player",
+    },
+  }
+
+  $("#gallery .button").on("click", function () {
+    fillModal(this.id)
+    $(".modal-wrap").addClass("visible")
+  })
+
+  $(".close").on("click", function () {
+    $(".modal-wrap, #modal .button").removeClass("visible")
+  })
+
+  $(".mask").on("click", function () {
+    $(".modal-wrap, #modal .button").removeClass("visible")
+  })
+
+  var carousel = $("#carousel"),
+    slideWidth = 700,
+    threshold = slideWidth / 3,
+    dragStart,
+    dragEnd
+
+  setDimensions()
+
+  $("#next").click(function () {
+    shiftSlide(-1)
+  })
+  $("#prev").click(function () {
+    shiftSlide(1)
+  })
+
+  carousel.on("mousedown", function () {
+    if (carousel.hasClass("transition")) return
+    dragStart = event.pageX
+    $(this).on("mousemove", function () {
+      dragEnd = event.pageX
+      $(this).css("transform", "translateX(" + dragPos() + "px)")
+    })
+    $(document).on("mouseup", function () {
+      if (dragPos() > threshold) {
+        return shiftSlide(1)
+      }
+      if (dragPos() < -threshold) {
+        return shiftSlide(-1)
+      }
+      shiftSlide(0)
+    })
+  })
+
+  function setDimensions() {
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    ) {
+      slideWidth = $(window).innerWidth()
+    }
+    $(".carousel-wrap, .slide").css("width", slideWidth)
+    $(".modal").css("max-width", slideWidth)
+    $("#carousel").css("left", slideWidth * -1)
+  }
+
+  function dragPos() {
+    return dragEnd - dragStart
+  }
+
+  function shiftSlide(direction) {
+    if (carousel.hasClass("transition")) return
+    dragEnd = dragStart
+    $(document).off("mouseup")
+    carousel
+      .off("mousemove")
+      .addClass("transition")
+      .css("transform", "translateX(" + direction * slideWidth + "px)")
+    setTimeout(function () {
+      if (direction === 1) {
+        $(".slide:first").before($(".slide:last"))
+      } else if (direction === -1) {
+        $(".slide:last").after($(".slide:first"))
+      }
+      carousel.removeClass("transition")
+      carousel.css("transform", "translateX(0px)")
+    }, 700)
+  }
+
+  function fillModal(id) {
+    $("#modal .title").text(modalText[id].title)
+    $("#modal .detail").text(modalText[id].detail)
+    $("#modal .tag").text(modalText[id].tag)
+    if (modalText[id].link)
+      $("#modal .button")
+        .addClass("visible")
+        .parent()
+        .attr("href", modalText[id].link)
+
+    $.each($("#modal li"), function (index, value) {
+      $(this).text(modalText[id].bullets[index])
+    })
+    $.each($("#modal .slide"), function (index, value) {
+      $(this).css({
+        background:
+          "url('img/slides/" + id + "-" + index + ".jpg') center center/cover",
+        backgroundSize: "cover",
+      })
+    })
+  }
+})
